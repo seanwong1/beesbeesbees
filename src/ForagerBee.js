17591f48dda3d2842e56530b3e60e52527ac5161
Sean Wong <sean.uuong@gmail.com>
2022-11-14 20:39:24 -0800

    Complete ForagerBee

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,16 @@
 var ForagerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.color = 'yellow';
+  this.job = 'find pollen';
+  this.honeyPot = 0;
+  this.canFly = true;
+  this.treasureChest = [];
 };
 
+ForagerBee.prototype = Object.create(Bee.prototype);
+ForagerBee.prototype.constructor = ForagerBee;
+
+ForagerBee.prototype.forage = function(treasure) {
+  this.treasureChest.push(treasure);
+}
