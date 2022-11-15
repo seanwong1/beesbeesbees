5566449f9ae298cc0df12b6d24c8abe7db701146
Sean Wong <sean.uuong@gmail.com>
2022-11-14 20:44:48 -0800

    Complete Bare Minimum Requirements

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,19 @@
 var RetiredForagerBee = function() {
+  ForagerBee.call(this);
+  this.age = 40;
+  this.color = 'grey';
+  this.job = 'gamble';
+  this.honeyPot = 0;
+  this.canFly = false;
 };
 
+RetiredForagerBee.prototype = Object.create(ForagerBee.prototype);
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
+
+RetiredForagerBee.prototype.forage = function() {
+  return 'I am too old, let me play cards instead';
+}
+
+RetiredForagerBee.prototype.gamble = function(treasure) {
+  this.treasureChest.push(treasure)
+}
